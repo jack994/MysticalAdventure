a1595a6b8425f9da1418c70035c1562568750597
giacomo <giacomobenso94@gmail.com>
2017-08-04 20:14:44 +0200

    solved libraries

--- asd/src/MysticalAdventure.java
+++ asd/src/MysticalAdventure.java
@@ -15,20 +15,20 @@ public class MysticalAdventure {
 	
 	public static void serializer(SavedObj obj) {
 		try {
-			FileOutputStream fileOut = new FileOutputStream("./folder/savedGame.ser");
+			FileOutputStream fileOut = new FileOutputStream("./lib/savedGame/savedGame.ser");
 			ObjectOutputStream out = new ObjectOutputStream(fileOut);
 			out.writeObject(obj);
 			out.close();
 			fileOut.close();
-			System.out.printf("Serialized data is saved in ./folder/savedGame.ser");
+			System.out.printf("Serialized data is saved in ./lib/savedGame/savedGame.ser");
 		} catch (IOException i) {
 			i.printStackTrace();
 		}
 	}
 
 	public static void die(){
-		File fileIn = new File("./folder/savedGameDeath.ser");
-		File fileOut = new File("./folder/savedGame.ser");
+		File fileIn = new File("./lib/savedGame/savedGameDeath.ser");
+		File fileOut = new File("./lib/savedGame/savedGame.ser");
 		try {
 			FileUtils.copyFile(fileIn, fileOut);
 		} catch (IOException e) {
@@ -50,7 +50,7 @@ public class MysticalAdventure {
 	
 	public static void deserializer(SavedObj obj, Game g) {
 		try {
-			FileInputStream fileIn = new FileInputStream("./folder/savedGame.ser");
+			FileInputStream fileIn = new FileInputStream("./lib/savedGame/savedGame.ser");
 			ObjectInputStream in = new ObjectInputStream(fileIn);
 			obj = (SavedObj) in.readObject();
 			in.close();
