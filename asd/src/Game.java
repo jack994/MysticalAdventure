53ec9de4efe5d5074889306a4f4f63d0512ff076
giacomo <giacomobenso94@gmail.com>
2017-10-02 12:49:35 +0100

    v1.0 it

--- asd/src/Game.java
+++ asd/src/Game.java
@@ -969,7 +969,7 @@ public class Game {
 		else{
 			code += "w";
 		}
-		code += "35bb"+ (rand.nextInt(4) + 3);
+		code += "35b"+ (rand.nextInt(4) + 3);
 		if(rand.nextBoolean()){
 			if(rand.nextBoolean()){
 				code += "rd";
@@ -987,6 +987,10 @@ public class Game {
 			}
 		}
 		code += (rand.nextInt(4) + 3);
+		code += "it";
+		for(int i=0; i<3; i++){
+			code+= (rand.nextInt(5) + 5);
+		}
 		
 		actione(code);
 		
